a1ba5e4101c8360cac9b53aea62efdc4797eae37
AhmetAkif07 <ahmetakifrbl@gmail.com>
2022-07-06 15:00:13 +0300

    hotel component eklendi

---
+++ src/app/components/add-hotel/add-hotel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-hotel',
+  templateUrl: './add-hotel.component.html',
+  styleUrls: ['./add-hotel.component.scss']
+})
+export class AddHotelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
