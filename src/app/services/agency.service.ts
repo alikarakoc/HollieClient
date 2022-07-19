8f3ed1fa42e8194f6f2523e053c4aa90e7a2092e
Sıla Soyat <sila.soyat10@gmail.com>
2022-07-19 17:19:22 +0300

    beggining of agency services

--- src/app/services/agency.service.ts
+++ src/app/services/agency.service.ts
@@ -1,19 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from "src/environments/environment";
 import { Agency } from "../interfaces";
+import { ListResponseModel } from '../interfaces/listResponseModel';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AgencyService {
   agencies: Agency[] = [];
+  baseUrl = `${environment.baseUrl}/Agency`;
 
-  baseUrl = `${environment.baseUrl}/`
+  constructor(private http: HttpClient) { }
 
-  // Buyrun efenim
-  addNewAgency() { }
+  getAllAgencies():Observable<ListResponseModel<Agency>>{
+    return this.http.get<ListResponseModel<Agency>>(`${this.baseUrl}/AllAgency`);
+  }
 
-  deleteAgency() { }
+  addNewAgency(agency: Partial<Agency>) {
+    return this.http.post<ListResponseModel<Agency>>(`${this.baseUrl}/add`, agency);
+   }
 
-  constructor() { }
+  deleteAgency(agency: Partial<Agency>) { 
+    return this.http.delete<ListResponseModel<Agency>>(`${this.baseUrl}/delete?Id=${agency.id}`);
+  }
+
+  updateAgency(agency: Partial<Agency>) {
+    return this.http.put<ListResponseModel<Agency>>(`${this.baseUrl}/update?Id=${agency.id}`, agency);
+  }
 }
