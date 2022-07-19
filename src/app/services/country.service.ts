46bc03190f8a7c106ee69f2319f006e4d923bb30
Sıla Soyat <sila.soyat10@gmail.com>
2022-07-19 22:02:01 +0300

    service updating

--- src/app/services/country.service.ts
+++ src/app/services/country.service.ts
@@ -1,20 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Country } from "../interfaces";
+import { Observable } from 'rxjs';
+import { environment } from "src/environments/environment";
+import { ListResponseModel } from '../interfaces/listResponseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
+
+  baseUrl = `${environment.baseUrl}/Country`;
   countries: Country[] = [];
 
-  addNewCountry(country: Country) {
+
+  getAllCountries(): Observable<ListResponseModel<Country>> {
+    return this.http.get<ListResponseModel<Country>>(`${this.baseUrl}/AllCountry`);
+  }
+  addNewCountry(country:Partial<Country>) {return this.http.post<ListResponseModel<Country>>(`${this.baseUrl}/add`, country);
   }
 
-  deleteCountry(country: Country) {
+  deleteCountry(country: Partial<Country>) {
+    return this.http.delete<ListResponseModel<Country>>(`${this.baseUrl}/delete?Id=${country.id}`);
   }
 
-  updateCountry(country: Country) {
+  updateCountry(country: Partial<Country>) {
+    return this.http.put<ListResponseModel<Country>>(`${this.baseUrl}/update?Id=${country.id}`, country);
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
