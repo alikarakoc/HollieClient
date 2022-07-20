d3b2004b0dcca516c5af19e77c288e9d81dc6854
AhmetAkif07 <66180709+AhmetAkif07@users.noreply.github.com>
2022-07-20 16:45:58 +0300

    hotel update done

--- src/app/components/hotel-update-dialog/hotel-update-dialog.component.ts
+++ src/app/components/hotel-update-dialog/hotel-update-dialog.component.ts
@@ -1,106 +1,67 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import {
-  MatDialog,
-  MatDialogRef,
-  MAT_DIALOG_DATA,
-} from '@angular/material/dialog';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { Hotel } from 'src/app/interfaces';
-import { HotelService } from 'src/app/services/hotel.service';
-import { HotelDeleteDialogComponent } from '../hotel-delete-dialog/hotel-delete-dialog.component';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { MatTable } from '@angular/material/table';
+import { Hotel } from "src/app/interfaces";
+import { HotelService } from "src/app/services";
+import { HotelDeleteDialogComponent } from "../hotel-delete-dialog/hotel-delete-dialog.component";
 import { TranslocoService } from '@ngneat/transloco';
 
 interface DialogData {
   element: Hotel;
-}
-
-type FormType<C> = FormControl<C | null>;
-
-interface FormData {
-  name: FormType<string>;
-  address: FormType<string>;
-  phone: FormType<string /* Phone */>;
-  email: FormType<string>;
+  table: MatTable<any>;
+  dialogRef: MatDialogRef<any>;
 }
 
 @Component({
   selector: 'app-hotel-update-dialog',
   templateUrl: './hotel-update-dialog.component.html',
-  styleUrls: ['./hotel-update-dialog.component.scss'],
+  styleUrls: ['./hotel-update-dialog.component.scss']
 })
 export class HotelUpdateDialogComponent implements OnInit {
-  formGroup: FormGroup<FormData> = new FormGroup<FormData>({
-    name: new FormControl<string>(this.data.element.name, [
-      Validators.required,
-      Validators.maxLength(50),
-    ]),
-    address: new FormControl<string>(this.data.element.address, [
-      Validators.required,
-      Validators.maxLength(200),
-    ]),
-    phone: new FormControl<string>(this.data.element.phone, [
-      Validators.required,
-      Validators.maxLength(20),
-    ]),
-    email: new FormControl<string>(this.data.element.email, [
-      Validators.required,
-      Validators.email,
-      Validators.maxLength(50),
-    ]),
-  });
+  newHotelName: string = this.data.element.name;
+  newHotelAddress: string = this.data.element.address;
+  newHotelPhone: string = this.data.element.phone;
+  newHotelEmail: string = this.data.element.email;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
     private dialogRef: MatDialogRef<HotelUpdateDialogComponent>,
     private hotelService: HotelService,
-    private dialog: MatDialog,
     private snackBar: MatSnackBar,
+    private dialog: MatDialog,
     public translocoService: TranslocoService
   ) { }
 
-  ngOnInit(): void { }
-
-  update() {
-    const predicate = (a: Hotel) =>
-      a.name === this.formGroup.value.name &&
-      a.address === this.formGroup.value.address &&
-      a.phone === this.formGroup.value.phone &&
-      a.email === this.formGroup.value.email;
-
-    const controls = {
-      nameControl: this.formGroup.controls.name,
-      emailControl: this.formGroup.controls.email,
-      addressControl: this.formGroup.controls.email,
-      phoneControl: this.formGroup.controls.phone,
-    };
-
-    const condition = this.hotelService.hotels.some(predicate);
-
-    if (condition) {
-      this.snackBar.open(this.translocoService.translate('dialogs.error_same', { name: this.translocoService.getActiveLang() === 'en' ? 'hotel' : 'otel' }), 'OK');
-      this.clearAreas();
-      return;
-    }
-
-    if (controls.emailControl!.hasError('email')) {
-      this.snackBar.open(this.translocoService.translate('dialogs.error_email'), 'OK');
-      return;
-    }
-
-    for (const control of Object.values(controls)) {
-      if (control.hasError('required')) {
-        this.snackBar.open(this.translocoService.translate('dialogs.error_required'), 'OK');
-        return;
-      }
-    }
-
-    this.snackBar.open(this.translocoService.translate('dialogs.update_success', { elementName: this.data.element.name }));
-    this.closeDialog();
+  ngOnInit(): void {
+    this.hotelService.getAllHotels().subscribe(res => {
+      this.hotels = res.data;
+    });
   }
 
-  clearAreas() {
-    // this.formGroup.setValue({ name: '', address: '', email: '', phone: '' });
+  hotels: Hotel[] = [];
+
+  update() {
+    if (!this.newHotelName) {
+      this.snackBar.open(this.translocoService.translate('dialogs.error_required'), "OK");
+      return;
+    }
+
+    if (this.hotels.some(c => c.name === this.newHotelName)) {
+      console.log(this.newHotelName);
+      this.snackBar.open(this.translocoService.translate('dialogs.error_same', { name: this.translocoService.getActiveLang() === 'en' ? 'hotel category' : 'otel tipi' }), "OK");
+      this.newHotelName = "";
+      return;
+    }
+
+    this.snackBar.open(this.translocoService.translate('dialogs.update_success'));
+    this.dialogRef.close({ isUpdated: true });
+    this.data.dialogRef?.close();
+    this.data.element.name = this.newHotelName;
+    this.hotelService.updateHotel(this.data.element);
+    console.log(this.data.element);
+    this.data.table?.renderRows();
+    this.closeDialog();
   }
 
   closeDialog() {
@@ -108,8 +69,18 @@ export class HotelUpdateDialogComponent implements OnInit {
   }
 
   delete() {
-    this.dialog.open(HotelDeleteDialogComponent, {
-      data: { element: this.data.element, dialogRef: this.dialogRef },
+    const dialog = this.dialog.open(HotelDeleteDialogComponent, {
+      data: { element: this.data.element, dialogRef: this.dialogRef }
+    });
+
+    dialog.afterClosed().subscribe(result => {
+      if (result.isDeleted) {
+        this.hotelService.deleteHotel({ name: this.newHotelName }).subscribe(() => {
+          this.ngOnInit();
+        });
+      }
     });
   }
+
 }
+
