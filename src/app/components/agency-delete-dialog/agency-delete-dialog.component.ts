a9ae0d7c0799816a8d1e7159b82baee030c048f8
Sıla Soyat <sila.soyat10@gmail.com>
2022-07-20 17:22:35 +0300

    needed commit

--- src/app/components/agency-delete-dialog/agency-delete-dialog.component.ts
+++ src/app/components/agency-delete-dialog/agency-delete-dialog.component.ts
@@ -3,6 +3,7 @@ import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 import { MatSnackBar } from "@angular/material/snack-bar";
 import { MatTable } from "@angular/material/table";
 import { Agency } from "src/app/interfaces";
+import { AgencyService } from "src/app/services";
 import { TranslocoService } from '@ngneat/transloco';
 
 interface DialogData {
@@ -22,16 +23,24 @@ export class AgencyDeleteDialogComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
     private dialogRef: MatDialogRef<AgencyDeleteDialogComponent>,
     private snackBar: MatSnackBar,
+    private agencyService: AgencyService,
     public translocoService: TranslocoService
   ) { }
 
   ngOnInit(): void {
   }
-
+  
   delete() {
     this.snackBar.open(this.translocoService.translate('dialogs.delete_success', { elementName: this.data.element.name }));
-    this.dialogRef.close();
+    this.closeDialog({ isDeleted : true});
     this.data.dialogRef.close();
+    this.agencyService.deleteAgency(this.data.element);
+    console.log("deleted agency");
+    console.log(this.data.element);
+    this.data.table?.renderRows();
   }
 
+  closeDialog({isDeleted} : { isDeleted : boolean;}) {
+    this.dialogRef.close({ isDeleted})
+  }
 }
