42d060e583133cd0133d7b04c07bdd14c8445d96
tuanaulker <tuanaaulker@gmail.com>
2022-07-21 16:33:10 +0300

    .

--- src/app/pages/hotel/hotel.component.ts
+++ src/app/pages/hotel/hotel.component.ts
@@ -18,6 +18,7 @@ export class HotelComponent implements OnInit {
   hotels: Hotel[] = [];
   hotelCategories: HotelCategory[] = [];
 
+  
   constructor(
     public hotelService: HotelService,
     private dialog: MatDialog,
