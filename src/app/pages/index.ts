d6399fcf6a07fa834ee53665750718e386fefff9
Efe YILDIRIM <vakeliefe@hotmail.com.tr>
2022-07-22 15:35:19 +0300

    BOARD'I HALLETTİM ARKADAŞLAR TŞK

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -5,3 +5,4 @@ export { AgencyComponent } from './agency/agency.component';
 export { HotelCategoryComponent } from './hotel-category/hotel-category.component';
 export { HotelComponent } from './hotel/hotel.component';
 export { MarketComponent } from './market/market.component'
+export { BoardComponent } from './board/board.component'
