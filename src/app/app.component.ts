a8ecc1f7c45df63784037652ef57da17f276b733
AhmetAkif07 <66180709+AhmetAkif07@users.noreply.github.com>
2022-07-25 17:36:39 +0300

    Dark Mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { OverlayContainer } from '@angular/cdk/overlay';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,4 +6,5 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+}
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { TranslocoService } from '@ngneat/transloco';
 
 @Component({
@@ -6,8 +8,28 @@ import { TranslocoService } from '@ngneat/transloco';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
-  constructor(private translocoService: TranslocoService) { }
+export class NavbarComponent implements OnInit {
+  toggleControl: FormControl = new FormControl(false);
+  @HostBinding('class') className = '';
+
+  ngOnInit(): void {
+    this.overlay.getContainerElement().classList.add("darkMode")
+    this.toggleControl.valueChanges.subscribe((darkMode) => {
+      const darkClassName = 'darkMode';
+      this.className = darkMode ? darkClassName : '';
+
+      if (darkMode) {
+        this.overlay.getContainerElement().classList.add(this.className);
+      } else {
+        this.overlay.getContainerElement().classList.remove(this.className);
+      }
+
+      console.log(this.overlay.getContainerElement().classList);
+    });
+  }
+
+  constructor(private translocoService: TranslocoService, private overlay: OverlayContainer) {
+  }
 
   changeLanguage(language: "tr" | "en") {
     this.translocoService.setActiveLang(language);
