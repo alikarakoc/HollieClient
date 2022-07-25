f8a1cd6c062d4f8edf2373156b8529b2c266adcc
AhmetAkif07 <66180709+AhmetAkif07@users.noreply.github.com>
2022-07-25 09:28:59 +0300

    Update currency.service.ts

--- src/app/services/currency.service.ts
+++ src/app/services/currency.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { environment } from "src/environments/environment";
+import { Currency } from "../interfaces";
+import { ListResponseModel } from '../interfaces/listResponseModel';
 @Injectable({
   providedIn: 'root'
 })
 export class CurrencyService {
+  baseUrl = `${environment.baseUrl}/Currency`;
+  currencies: Currency[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllCurrency(): Observable<ListResponseModel<Currency>> {
+    return this.http.get<ListResponseModel<Currency>>(`${this.baseUrl}/AllCurrency`);
+  }
+
+  addCurrency(currency: Partial<Currency>) {
+    return this.http.post<ListResponseModel<Currency>>(`${this.baseUrl}/add`, currency);
+  }
+
+  deleteCurrency(currency: Partial<Currency>) {
+    return this.http.delete<ListResponseModel<Currency>>(`${this.baseUrl}/delete?Id=${currency.id}`);
+  }
+
+  updateCurrency(currency: Partial<Currency>) {
+    return this.http.put<ListResponseModel<Currency>>(`${this.baseUrl}/update?Id=${currency.id}`, currency);
+  }
 }
