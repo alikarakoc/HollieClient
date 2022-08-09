d5e84dd15a867b07c86da9f146c829d9644f8791
Efe YILDIRIM <vakeliefe@hotmail.com.tr>
2022-08-09 15:54:04 +0300

    index sayfası

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AgencyComponent, CountryComponent, ErrorComponent, HotelCategoryComponent, RoomTypeComponent,HotelComponent, MarketComponent, BoardComponent, CurrencyComponent, ContractComponent, SearchContractComponent } from "./pages";
 import { HomeComponent } from "./pages/home/home.component";
+import { IndexComponent } from './pages/index/index.component';
 import { RoomComponent } from './pages/room/room.component';
 
 const routes: Routes = [
@@ -40,6 +41,10 @@ const routes: Routes = [
   {
     path: "market",
     component: MarketComponent
+  },
+  {
+    path: "index",
+    component: IndexComponent
   },
   {
     path: "currencies",
---
+++ src/app/pages/index/index.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+    selector: 'app-index',
+    templateUrl: './index.component.html',
+    styleUrls: ['./css/bootstrap.min.css','./css/datepicker.css','./css/tooplate-style.css','./font-awesome-4.7.0/css/font-awesome.min.css','slick/slick.css','slick/slick-theme.css'],
+    
+  })
+
+export class IndexComponent implements OnInit {
+
+    ngOnInit(): void {
+    }
+}
